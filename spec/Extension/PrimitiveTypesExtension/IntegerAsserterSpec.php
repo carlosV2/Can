a96b2ec982d70db1cbb5c0e9c28bc358faa5a93f
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2016-05-28 10:17:04 +0100

    Create the integer asserter

---
+++ spec/Extension/PrimitiveTypesExtension/IntegerAsserterSpec.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace spec\carlosV2\Can\Extension\PrimitiveTypesExtension;
+
+use carlosV2\Can\Asserter;
+use PhpSpec\ObjectBehavior;
+
+class IntegerAsserterSpec extends ObjectBehavior
+{
+    function it_is_an_Asserter()
+    {
+        $this->shouldHaveType(Asserter::class);
+    }
+
+    function it_returns_true_for_integer_numbers()
+    {
+        $this->check(123)->shouldReturn(true);
+    }
+
+    function it_returns_false_for_anything_but_integer_numbers()
+    {
+        $this->check('abc')->shouldReturn(false);
+        $this->check(null)->shouldReturn(false);
+        $this->check(12.3)->shouldReturn(false);
+        $this->check([])->shouldReturn(false);
+        $this->check(true)->shouldReturn(false);
+    }
+
+    function it_ensures_the_integer_has_a_minimum_value()
+    {
+        $this->withMin(123)->shouldReturn($this);
+
+        $this->check(122)->shouldReturn(false);
+        $this->check(123)->shouldReturn(true);
+        $this->check(124)->shouldReturn(true);
+    }
+
+    function it_ensures_the_integer_has_a_maximum_value()
+    {
+        $this->withMax(123)->shouldReturn($this);
+
+        $this->check(122)->shouldReturn(true);
+        $this->check(123)->shouldReturn(true);
+        $this->check(124)->shouldReturn(false);
+    }
+}
---
+++ src/Extension/PrimitiveTypesExtension/IntegerAsserter.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace carlosV2\Can\Extension\PrimitiveTypesExtension;
+
+use carlosV2\Can\Asserter;
+
+class IntegerAsserter implements Asserter
+{
+    /**
+     * @var int|null
+     */
+    private $min;
+
+    /**
+     * @var int|null
+     */
+    private $max;
+
+    /**
+     * @param int $min
+     *
+     * @return IntegerAsserter
+     */
+    public function withMin($min)
+    {
+        $this->min = $min;
+
+        return $this;
+    }
+
+    /**
+     * @param int $max
+     *
+     * @return IntegerAsserter
+     */
+    public function withMax($max)
+    {
+        $this->max = $max;
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function check($data)
+    {
+        if (!is_int($data)) {
+            return false;
+        }
+
+        if (isset($this->min)) {
+            if ($data < $this->min) {
+                return false;
+            }
+        }
+
+        if (isset($this->max)) {
+            if ($data > $this->max) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
