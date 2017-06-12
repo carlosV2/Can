8cdf458f9d4320723c9edf3916614ba139069394
Carlos Ortega <carlosV2.0@gmail.com>
2017-06-12 23:37:13 +0100

    Add IDE helpers

--- src/To.php
+++ src/To.php
@@ -3,7 +3,33 @@
 namespace carlosV2\Can;
 
 use carlosV2\Can\Exception\AsserterNotFoundException;
+use carlosV2\Can\Extension\ConstraintsExtension\AllOfAsserter;
+use carlosV2\Can\Extension\ConstraintsExtension\OneOfAsserter;
+use carlosV2\Can\Extension\ConstraintsExtension\OptionalAndAsserter;
+use carlosV2\Can\Extension\PrimitiveTypesExtension\ArrayAsserter;
+use carlosV2\Can\Extension\PrimitiveTypesExtension\BooleanAsserter;
+use carlosV2\Can\Extension\PrimitiveTypesExtension\CallableAsserter;
+use carlosV2\Can\Extension\PrimitiveTypesExtension\FloatAsserter;
+use carlosV2\Can\Extension\PrimitiveTypesExtension\IntegerAsserter;
+use carlosV2\Can\Extension\PrimitiveTypesExtension\NullAsserter;
+use carlosV2\Can\Extension\PrimitiveTypesExtension\ObjectAsserter;
+use carlosV2\Can\Extension\PrimitiveTypesExtension\StringAsserter;
 
+/**
+ * @method static StringAsserter beString()
+ * @method static IntegerAsserter beInteger()
+ * @method static IntegerAsserter beInt()
+ * @method static FloatAsserter beFloat()
+ * @method static ArrayAsserter beArray()
+ * @method static NullAsserter beNull()
+ * @method static BooleanAsserter beBool()
+ * @method static BooleanAsserter beBoolean()
+ * @method static CallableAsserter beCallable()
+ * @method static ObjectAsserter beObject()
+ * @method static OptionalAndAsserter beOptionalAnd(AsserterInterface $asserter)
+ * @method static AllOfAsserter beAllOf(AsserterInterface ...$asserters)
+ * @method static OneOfAsserter beOneOf(AsserterInterface ...$asserters)
+ */
 class To
 {
     /**
